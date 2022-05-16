39456f46aa9336b5b8175d58022ca2d31b7b9db7
Grace Nyutu <graceynyutu@gmail.com>
2022-05-16 12:01:00 +0300

    add pitches and create for loop to display the pitches

--- pitches.py
+++ pitches.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+pitches = [
+    {
+        'author': 'Barrack Obama',
+        'type': 'One Word Pitch',
+        'content': 'Hope',
+        'date_posted': 'May 13, 2022'
+    },
+    {
+        'author': 'Google',
+        'type': 'One Word Pitch',
+        'content': 'Search',
+        'date_posted': 'May 1, 2022'
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', pitches=pitches)
 
 @app.route("/about")
 def about():
