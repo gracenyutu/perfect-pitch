a3ed9d72fc04945ef72db7b8b6741c6c0a1c69ed
Grace Nyutu <graceynyutu@gmail.com>
2022-05-16 21:33:15 +0300

    edit nav-links to use url_for

--- pitches.py
+++ pitches.py
@@ -1,9 +1,13 @@
 # from title import title
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '99e131c1b2aa8de8303e746b92bd7faa486cd99b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
 
 pitches = [
     {
