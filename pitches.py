1cea7a1e06ca9752686239fab83d92f1eec2738d
Grace Nyutu <graceynyutu@gmail.com>
2022-05-17 15:08:10 +0300

    create models.py file and move the model classes therein

--- pitches.py
+++ pitches.py
@@ -1,34 +1,15 @@
 # from title import title
-from datetime import datetime
 from email.policy import default
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+from models import User, Pitch
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '99e131c1b2aa8de8303e746b92bd7faa486cd99b'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    username = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    pitches = db.relationship('Post', backref='author', lazy=True)
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
-class Pitch(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-
-    def __repr__(self):
-        return f"Pitch('{self.type}', '{self.date_posted}')"
 
 pitches = [
     {
